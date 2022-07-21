7e765a4c4497641a984853c8dde7b8d36e17f583
rainer991 <rainer991@gmail.com>
2022-07-21 16:12:53 -0400

    Icons Added

--- src/components/box/box.jsx
+++ src/components/box/box.jsx
@@ -1,26 +1,83 @@
 import "./box.css";
-import thumbnail from '../../assets/images/animation.png'
+import thumbnail from "../../assets/images/animation.png";
+import thumbnail2 from "../../assets/images/design.png";
+import thumbnail3 from "../../assets/images/photo.png";
+import thumbnail4 from "../../assets/images/crypto.png";
+import thumbnail5 from "../../assets/images/business.png";
+
 
 const FirstBox = () => {
-    return(
-        <div className='first'>
-            <h4 className="first-info">
-            Check out our most popular courses!
-            </h4>
-        </div>
-    );
-}
+  return (
+    <div className="first">
+      <h4 className="first-info">Check out our most popular courses!</h4>
+    </div>
+  );
+};
 
 const BoxItem = () => {
-    return(
-        <div className='item'>
-            <img className="icon" src={thumbnail}/>
-            <h6 className="">Animation</h6>
-            <p className="">Learn the latest animation techniques to create stunning motion design and captivate your audience.</p>
-            <a className="">Get Started</a>
-        </div>
-    );
-}
+  return (
+    <div className="item">
+      <img className="icon" src={thumbnail} />
+      <h6 className="h-text">Animation</h6>
+      <p className="p-text">
+        Learn the latest animation techniques to create stunning motion design
+        and captivate your audience.
+      </p>
+      <a className="item-color">Get Started</a>
+    </div>
+  );
+};
+const BoxItem2 = () => {
+  return (
+    <div className="item">
+      <img className="icon" src={thumbnail2} />
+      <h6 className="h-text">Design</h6>
+      <p className="p-text">
+        Design Create beautiful, usable interfaces to help shape the future of
+        how the web looks. 
+      </p>
+      <a className="item-color">Get Started</a>
+    </div>
+  );
+};
+const BoxItem3 = () => {
+  return (
+    <div className="item">
+      <img className="icon" src={thumbnail3} />
+      <h6 className="h-text">Photography</h6>
+      <p className="p-text">
+        Photography Explore critical fundamentals like lighting, composition,
+        and focus to capture exceptional photos. 
+      </p>
+      <a className="item-color">Get Started</a>
+    </div>
+  );
+};
+const BoxItem4 = () => {
+  return (
+    <div className="item">
+      <img className="icon" src={thumbnail4} />
+      <h6 className="h-text">Crypto</h6>
+      <p className="p-text">
+        Crypto All you need to know to get started investing in crypto. Go from
+        beginner to advanced with this 54 hour course. 
+      </p>
+      <a className="item-color">Get Started</a>
+    </div>
+  );
+};
+const BoxItem5 = () => {
+  return (
+    <div className="item">
+      <img className="icon" src={thumbnail5} />
+      <h6 className="h-text">Bussiness</h6>
+      <p className="p-text">
+        Business A step-by-step playbook to help you start, scale, and sustain
+        your business without outside investment. 
+      </p>
+      <a className="item-color">Get Started</a>
+    </div>
+  );
+};
 
-
-export {FirstBox, BoxItem}
+export { FirstBox, BoxItem, BoxItem2, BoxItem3, BoxItem4, BoxItem5 };
--- src/components/layout/components/body/body.jsx
+++ src/components/layout/components/body/body.jsx
@@ -1,12 +1,12 @@
 import "./body.css";
 import { Button3 } from "../../../commons/button/button";
-import { FirstBox, BoxItem } from "../../../box/box";
+import { FirstBox, BoxItem, BoxItem2, BoxItem3, BoxItem4, BoxItem5 } from "../../../box/box";
 
 const Body = () => {
   return (
     <div className="hero">
       <div className="info">
-        <h1 className="">Maximize skill, minimize budget</h1>
+        <h1 className="h-size">Maximize skill, minimize budget</h1>
         <p className="">
           Our modern courses across a range of in-demand skills will give you
           the knowledg e you need to live the life you want.
@@ -17,10 +17,10 @@ const Body = () => {
       <div className="items">
         <FirstBox/>
         <BoxItem/>
-        <BoxItem/>
-        <BoxItem/>
-        <BoxItem/>
-        <BoxItem/>
+        <BoxItem2/>
+        <BoxItem3/>
+        <BoxItem4/>
+        <BoxItem5/>
       </div>
     </div>
   );
